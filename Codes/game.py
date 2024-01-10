eafe10f64dbf15206fb3252c12b08e93c1e4a23a
João Marcelo Costa de Santana <joaomarcelocosta14@gmail.com>
2024-01-09 22:14:00 -0300

    nada

---
+++ Codes/game.py
@@ -0,0 +1,151 @@
+import pygame
+from menu import *
+from random import randint
+"""
+Arquivo com funções usadas enquanto o jogo roda.
+"""
+
+def escolhe_cor(element):
+    """
+    Função que escolhe a cor de acordo com o elemento do jogo (inimigo, jogador, bala ou combustível)
+    """
+    if  element == ' ':
+        cor = (255, 255, 255)
+    elif element == '+':
+        cor = (0, 255, 0)
+    elif element == 'X':
+        cor = (255, 0, 0)
+    elif element == '>':
+        cor = (0, 100, 0)
+    elif element == 'F':
+        cor = (0, 0, 100)
+
+    return cor
+
+def desenhar(screen, cor, x, y, size):
+    """"
+    Função que desenha um elemento específico na tela do jogo
+    """
+    pygame.draw.rect(screen, cor, (x * size,
+    y * size, size, size))
+
+def mostrar_matriz(matriz, altura, largura, tela, tamanho, pont, energ, fonte):
+    """
+    Função que desenha todo o jogo na tela
+    """
+    tela.fill((255, 255, 255))
+
+    for i in range(0, altura):
+        for j in range(0, largura):
+                    desenhar(tela, escolhe_cor(matriz[i][j]), j, i, tamanho) # desenho
+
+    escrever(f'Energia: {energ}', tela, fonte, (0,0,0), 3, 0, tamanho)
+    escrever(f'Pontuação: {pont}', tela, fonte, (0,0,0), 600, 0, tamanho)
+    pygame.display.flip()
+
+def mover_objetos(matriz, altura, largura, pont, energ):
+    """
+    função que move os objetos presentes no jogo(inimigos, balas e combustível)
+    """
+
+    # movimentação dos tiros
+    i = 0
+    j = 0
+    while i < altura:
+        while j < largura:
+            if matriz[i][j] == '>':
+                    if j < (largura - 1):
+                        if matriz[i][j + 1] == ' ':
+                            matriz[i][j + 1] = '>'
+                            matriz[i][j] = ' '
+                            j += 1
+
+                        elif matriz[i][j + 1] == 'X' or matriz[i][j + 1] == 'F':
+                            if matriz[i][j + 1] == 'X':
+                                 pont += 50
+                            matriz[i][j + 1] = ' '
+                            matriz[i][j] = ' '
+
+                    elif j == (largura - 1):
+                        matriz[i][j] = ' '
+            j += 1
+        j = 0
+        i += 1
+
+    # movimentação dos inimigos e do combustível
+    i = 0
+    j = 0
+    while i < altura:
+        while j < largura:
+            if matriz[i][j] == 'X':
+                    if j > 0:
+                        if matriz[i][j - 1] == ' ':
+                            matriz[i][j - 1] = 'X'
+                            matriz[i][j] = ' '
+                            j += 1
+
+                        elif matriz[i][j - 1] == '+': # morte do player
+                            matriz[i][j - 1] = ' '
+                            matriz[i][j] = ' '
+
+                    elif j == 0:
+                        matriz[i][j] = ' '
+            elif matriz[i][j] == 'F':
+                    if j > 0:
+                        if matriz[i][j - 1] == ' ':
+                            matriz[i][j - 1] = 'F'
+                            matriz[i][j] = ' '
+                            j += 1
+                        elif matriz[i][j - 1] == '+': # aumento do combustivel
+                            energ += 40
+                            matriz[i][j] = ' '
+                            j += 1
+                    elif j == 0:
+                        matriz[i][j] = ' '
+            j += 1
+        j = 0
+        i += 1
+
+    # retorno da matriz e dos valores da energia e pontuação após a movimentação    
+    return matriz, pont, energ
+
+def spawn(matriz, objeto, prob, limpar, qtde_max):
+    """
+    Função que spawna aleatoriamente inimigos ou combustível no mapa
+    OBS: chance em porcentagem
+    """
+    b = randint(0, 100) 
+    if b <= prob:
+        qtde = randint(0, qtde_max) # qtde de inimigos que vão aparecer
+        
+        if qtde > 0:
+                usadas = []
+                for i in range(0, qtde):
+                    while True:
+                        y = randint(0, 9)
+
+                        if y not in usadas:
+                            matriz[y][134] = objeto 
+                            usadas.append(y)
+                            break
+                if limpar:
+                    del usadas
+
+def limpar_matriz(grid, h, l):
+     """
+     Função para a limpeza da matriz para o possível início
+     de uma futura partida
+     """
+     for i in range(0, h):
+          for j in range(0, l):
+               grid[i][j] = ' '
+
+def resetar_contador(c, e):
+    """
+    Função que reseta o contador de segundos a cada segundo
+    e diminui a energia do personagem em 1.
+    """
+    if c == 20:
+        c = 0 # reseta o contador
+        e -= 1 # diminuição da energia do personagem a cada frame
+    return c, e
---
+++ Codes/main.py
@@ -0,0 +1,240 @@
+"""
+Universidade de Brasilia
+Instituto de Ciencias Exatas
+Departamento de Ciencia da Computacao
+Algoritmos e Programação de Computadores - 2/2023
+Turma: Prof. Carla Castanho e Prof. Frank Ned
+Aluno(a): João Marcelo Costa de Santana
+Matricula: 232023790
+Projeto Final - Parte 1
+Descricao: Projeto desenvolvido utilizando os conceitos aprendidos em aula e
+as bibliotecas pygame e random. O projeto consiste num jogo 2D onde o jogador 
+movimenta um boneco para cima e para baixo e também pode atirar. Inimigos e
+células de combustível surgem constantemente no mapa e vão da esquerda para 
+a direita. O objetivo do jogador é desviar dos inimigos e ao mesmo tempo coletar
+o máximo de combustível que puder e atirar nos inimigos para aumentar sua pontuação.
+"""
+
+from menu import *
+from game import *
+from gameover import *
+import pygame
+
+#  definição das variaveis utilizadas no jogo
+nome_do_jogo = 'GTA VII'
+fonte = "monospace"
+
+fps = 20 # fps do jogo
+contador = 0 # contador que aumenta a cada frame, quando chega em 20 o combustivel diminui
+enter = 13 # codigo do enter no pygame
+inicio = True # variavel que indica que o jogo ainda esta na tela inicial 
+menu_inicial = False # variavel que indica se o jogo esta no menu 
+playing = False # variavel que indica se o jogo ta rodando
+game_overI = False # variavel que indica se o jogo esta na tela de game over
+game_overII = False
+instrucoes = False
+paused = False
+
+tam_jogo = 15
+largura_jogo = 135
+altura_jogo = 10
+tam_menu = 5 # tamanho da tela
+largura_menu = 72 * tam_menu
+altura_menu = 128 * tam_menu
+
+# Lista bidimensional para armazenar objetos em movimento
+matriz = [[' ' for _ in range(largura_jogo)] for _ in range(altura_jogo)]
+
+# cores utilizadas 
+vermelho = (255, 0, 0)
+branco = (255, 255, 255)
+preto = (0, 0, 0)
+verde = (0, 255, 0)
+azul = (0, 0, 255)
+verde_esc = (0, 100, 0)
+
+# Definição de objetos no jogo
+player = '+'
+enemy = 'X'
+comb = 'F'
+bala = '>'
+
+# energia inicial do personagem
+energ = 400
+
+#probabilidades de serem gerados
+prob_enemies = 25
+prob_fuel = 5
+
+# pontuação do jogador
+pont = 0
+
+# coordenadas iniciais do player
+player_x = 1
+player_y = 5
+matriz[player_y][player_x] = '+'
+
+
+pygame.init()
+
+# configuracao do fps
+clock = pygame.time.Clock()
+
+screen = pygame.display.set_mode((altura_menu, largura_menu)) # tamanho da tela inicial
+pygame.display.set_caption(nome_do_jogo)
+
+# escrita da msg inicial ao abrir o jogo
+escrever(nome_do_jogo, screen, fonte, vermelho, 200, 135, tam_menu * 10)
+escrever('Bem vindo! Pressione Enter para continuar', screen, fonte, branco, 180, 250, tam_menu * 2)
+pygame.display.flip()
+
+running = True
+while running:
+
+    # loop para eventos no jogo
+    if inicio: # quando esta no inicio do jogo
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: # pygame.QUIT event means the user clicked X to close your window
+                running = False
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == 13:
+                    inicio, menu_inicial = False, True # o jogo nao esta mais no inicio
+                    menu(screen, tam_menu, largura_menu, altura_menu, fonte) # função que apresentará o menu inicial do game
+
+    if menu_inicial: # quando está no menu inicial
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: # pygame.QUIT event means the user clicked X to close your window
+                running = False    
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_5: # 5 pra sair
+                    running = False
+
+                elif event.key == pygame.K_1: # 1 pra jogar
+                    playing, menu_inicial = True, False
+                    tela_jogo = pygame.display.set_mode((largura_jogo * tam_jogo, altura_jogo * tam_jogo))
+
+                elif event.key == pygame.K_4:
+                    instrucoes, menu_inicial = True, False
+                    mostrar_instrucoes(altura_menu, largura_menu, preto, branco, tam_menu, fonte)
+
+    if instrucoes:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: # pygame.QUIT event means the user clicked X to close your window
+                running = False
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == 13:
+                    menu_inicial, instrucoes = True, False # o jogo nao esta mais no inicio
+                    menu(screen, tam_menu, largura_menu, altura_menu, fonte) # função que apresentará o menu inicial do game
+
+
+
+    if playing: # quando o jogo está rodando
+
+        contador, energ = resetar_contador(contador,energ) # reset do contador e da energia
+        
+        #movimentação dos objetos e atualização dos frames
+        matriz, pont, energ = mover_objetos(matriz, altura_jogo, largura_jogo, pont, energ)
+
+        # verificação da morte do personagem e do motivo dela 
+        morte = morreu(matriz, energ, player_y)
+        motivo = motivo_morte(morte)
+        if 'sim' in morte:
+            game_overI, playing = True, False # encerramento da partida em caso de morte do jogador
+
+        mostrar_matriz(matriz, altura_jogo, largura_jogo,tela_jogo, tam_jogo, pont, energ, fonte)
+        contador += 1
+
+        # spawn de inimigos e combustível
+        spawn(matriz, enemy, prob_enemies, False, 4)
+        spawn(matriz, comb, prob_fuel, True, 2)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: # pygame.QUIT event means the user clicked X to close your window
+                running = False
+
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP: # movimentação do personagem para cima
+                    if player_y > 0:    
+                        matriz[player_y][player_x] = ' '
+                        matriz[player_y - 1][player_x] = player
+                        player_y -= 1
+                        energ -= 2
+
+                elif event.key == pygame.K_DOWN: # movimentação do personagem para baixo
+                    if player_y < 9:    
+                        matriz[player_y][player_x] = ' '
+                        matriz[player_y + 1][player_x] = player
+                        player_y += 1
+                        energ -= 2
+                
+                elif event.key == pygame.K_SPACE: # geração do tiro
+                    bala_y = player_y
+                    bala_x = player_x + 1
+                    matriz[bala_y][bala_x] = bala
+                    energ -= 3
+
+                elif event.key == pygame.K_p:
+                    paused, playing = True, False
+                
+                elif event.key == 13: # Enter dropa do jogo
+                    menu_inicial = True
+                    playing = False
+
+                    # reset das configs do jogo para uma futura partida
+                    contador, energ, pont = 0, 400, 0
+                    limpar_matriz(matriz, altura_jogo, largura_jogo)
+                    player_x, player_y = 1, 5
+                    matriz[player_y][player_x] = '+'
+                    screen = pygame.display.set_mode((altura_menu, largura_menu))
+                    menu(screen, tam_menu, largura_menu, altura_menu, fonte) # função que apresentará o menu inicial do game
+                    pygame.display.flip()
+
+
+    if game_overI:
+        tela_morte(largura_menu, altura_menu, fonte, tam_menu, pont, motivo)
+        
+        # reset das configs do jogo para uma futura partida
+        contador, energ, pont = 0, 400, 0
+        limpar_matriz(matriz, altura_jogo, largura_jogo)
+        player_x, player_y = 1, 5
+        matriz[player_y][player_x] = '+'
+        game_overI, game_overII = False, True
+
+    if game_overII:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: # pygame.QUIT event means the user clicked X to close your window
+                running = False
+            
+            if event.type == pygame.KEYDOWN:
+                if event.key == 13:
+                    game_over, menu_inicial = False, True # o jogo nao esta mais no inicio
+                    menu(screen, tam_menu, largura_menu, altura_menu, fonte) # função que apresentará o menu inicial do game
+                    pygame.display.flip()
+
+    if  paused:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_p:
+                    paused, playing = False, True
+
+                elif event.key == 13: # Enter dropa do jogo
+                    menu_inicial = True
+                    playing = False
+
+                    # reset das configs do jogo para uma futura partida
+                    contador, energ, pont = 0, 400, 0
+                    limpar_matriz(matriz, altura_jogo, largura_jogo)
+                    player_x, player_y = 1, 5
+                    matriz[player_y][player_x] = '+'
+                    screen = pygame.display.set_mode((altura_menu, largura_menu))
+                    menu(screen, tam_menu, largura_menu, altura_menu, fonte) # função que apresentará o menu inicial do game
+                    pygame.display.flip()
+
+
+
+        
+    clock.tick(fps)
+    
+pygame.quit()
